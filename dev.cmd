2bc10426c4149dd8a2911dce3eaab061d653ed01
makoto <patter891013@gmail.com>
2026-01-05 10:29:51 +0800

    刪除 dev.cmd 檔案

--- dev.cmd
+++
@@ -1,2 +0,0 @@
-powershell -Command "(gc .env.local) -replace 'GEMINI_API_KEY=PLACEHOLDER_API_KEY', 'GEMINI_API_KEY=AIzaSyCDKzkJUQExCH36e0gb2O3vNxQ8GJxoIGA' | Out-File -encoding utf8 .env.local"
-npm install&&npm run dev
